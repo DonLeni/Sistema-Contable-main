261382ca8dd49b2604d55af3bf5451e14d42032c
Don Lenni <leninavilez022@gmail.com>
2024-07-12 22:48:14 -0600

    se modfico vista cuentas

--- resources/views/Mantenimiento/Cuentas.blade.php
+++ resources/views/Mantenimiento/Cuentas.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Villas Las Acacias')
 
 @section('content_header')
-    <h1>Cuentas</h1>
+    <h1>Catalogo de Cuentas</h1>
 
 @stop
 
@@ -11,7 +11,7 @@
     <div class="card">
         <div class="card-body">
             <div class="table-responsive">
-                <table id="Cuenta" class="table">
+                <table id="Catalogo" class="table">
                     <thead class="thead-info">
                         <tr>
                             <th>Codigo ID</th>
@@ -19,20 +19,24 @@
                             <th>Nombre</th>
                             <th>Descripcion</th>
                             <th>Tipo de Cuenta</th>
+                            <th>Cuenta Origen</th>
+                            <th>Usuario</th>
                             <th class="d-none d-sm-table-cell">Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($ResulCuenta as $Cuenta)
+                        @foreach($ResulCatalogo as $Catalogo)
                             <tr>
-                                <td>{{ $Cuenta['CTA_ID'] }}</td>
-                                <td>{{ $Cuenta['CTA_CODIGO'] }}</td>
-                                <td>{{ $Cuenta['CTA_NOMBRE'] }}</td>
-                                <td>{{ $Cuenta['CTA_DESCRIPCION'] }}</td>
-                                <td>{{ $Cuenta['CTA_TIPO'] }}</td>
+                                <td>{{ $Catalogo['ID_CUENTA'] }}</td>
+                                <td>{{ $Catalogo['CODIGO'] }}</td>
+                                <td>{{ $Catalogo['NOMBRE'] }}</td>
+                                <td>{{ $Catalogo['DESCRIPCION'] }}</td>
+                                <td>{{ $Catalogo['TIPO_CUENTA'] }}</td>
+                                <td>{{ $Catalogo['NOMBRE_PADRE'] }}</td>
+                                <td>{{ $Catalogo['Usuario'] }}</td>
                                 <td class="d-none d-sm-table-cell">
-                                    <a href="{{ url('/UpdateFormTel/'.$Cuenta['CTA_ID']) }}" class="btn btn-info editar-btn">Editar</a>
-                                    <form method="POST" action="{{ url('/EliminarCuenta/'.$Cuenta['CTA_ID']) }}" style="display: inline;">
+                                    <a href="{{ url('/UpdateFormTel/'.$Catalogo['ID_CUENTA']) }}" class="btn btn-info editar-btn">Editar</a>
+                                    <form method="POST" action="{{ url('/EliminarCatalogo/'.$Catalogo['ID_CUENTA']) }}" style="display: inline;">
                                         @method('DELETE')
                                         @csrf
                                         <button type="submit" class="btn btn-danger borrar-btn">Borrar</button>
@@ -61,7 +65,7 @@
     <script src="https://cdn.datatables.net/2.0.4/js/dataTables.bootstrap5.js"></script>
     <script>
         $(document).ready(function() {
-            $('#Cuenta').DataTable({
+            $('#Catalogo').DataTable({
                 language: {
                     lengthMenu: "Mostrar _MENU_ registros por página",
                     zeroRecords: "Ningún objeto encontrado",
